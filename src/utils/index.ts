afe5041e5a66470b5bef60ea197d8e24d6028eb8
ncedile <ncedile@yoco.co.za>
2020-11-27 16:56:50 +0200

    Implement sharing functionality for the to do list items

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,25 @@
 /**
  * Generates a random id between A0 and Aseed
  */
+import {Todo} from '../services/appState/todosList/types';
+
 const generateItemID = (seed: number = 10000000) =>
   `A${Math.floor(Math.random() * Math.floor(seed))}`;
 
-export {generateItemID};
+/**
+ * Convert list items to text message for sharing
+ */
+
+const sharingContent = (todoItems: Todo[]) => {
+  let shareContent = 'Todo items as follows:\n';
+  todoItems.forEach((todoItem) => {
+    shareContent =
+      shareContent +
+      `Name: \n${todoItem.name}\nDescription: ${
+        todoItem.description
+      }\nStatus: ${todoItem.isDone ? 'Completed' : 'Not done'}\n\n`;
+  });
+  return shareContent;
+};
+
+export {generateItemID, sharingContent};
